d8c93bd073976a07a45a77db7fa1e25099920be7
Mateus CohuzEr <52006006+MateusCohuzer@users.noreply.github.com>
2023-09-25 18:59:28 -0300

    im super shy

--- src/java/servlets/redirect.java
+++ src/java/servlets/redirect.java
@@ -32,7 +32,7 @@ public class redirect extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             HttpSession session = request.getSession();
-            session.setAttribute("email", "matebatichotti@gmail.com");
+            session.setAttribute("email", "aaa@gmail.com");
             response.sendRedirect("index.jsp");
         }
     }
