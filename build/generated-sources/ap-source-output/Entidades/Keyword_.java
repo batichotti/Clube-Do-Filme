d6fc6040efbcd433baa8df0223f6937412b7fc00
Mateus CohuzEr <52006006+MateusCohuzer@users.noreply.github.com>
2023-09-26 11:36:27 -0300

    update login

--- build/generated-sources/ap-source-output/Entidades/Keyword_.java
+++ build/generated-sources/ap-source-output/Entidades/Keyword_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-09-25T18:50:54", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-09-26T11:32:40", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Keyword.class)
 public class Keyword_ { 
 
