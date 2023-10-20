23fb25f3bebef15523e32d983cc9e07f1ca857ab
Matt Batichotti <52006006+MateusCohuzer@users.noreply.github.com>
2023-10-20 08:28:54 -0300

    .

--- build/generated-sources/ap-source-output/Entidades/ProductionCountry_.java
+++ build/generated-sources/ap-source-output/Entidades/ProductionCountry_.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-17T11:23:24", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-10-20T08:28:37", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(ProductionCountry.class)
 public class ProductionCountry_ { 
 
