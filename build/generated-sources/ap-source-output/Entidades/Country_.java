fa2fec52ea68362f9700b56ba90dc51771c2521e
Matt Batichotti <52006006+MateusCohuzer@users.noreply.github.com>
2023-12-12 18:02:17 -0300

    .

--- build/generated-sources/ap-source-output/Entidades/Country_.java
+++ build/generated-sources/ap-source-output/Entidades/Country_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-11-26T21:31:43", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-12-01T10:19:33", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Country.class)
 public class Country_ { 
 
--- build/generated-sources/ap-source-output/Entidades/MovieGenres_.java
+++ build/generated-sources/ap-source-output/Entidades/MovieGenres_.java
@@ -7,7 +7,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-11-26T21:31:43", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-12-01T10:19:33", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(MovieGenres.class)
 public class MovieGenres_ { 
 
